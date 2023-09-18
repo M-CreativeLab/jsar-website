517e58fb47dada9533d991f6e6f4bb6d06f1b57c
Yorkie Makoto <yorkiefixer@gmail.com>
2023-09-19 01:15:36 +0800

    remove asset prefix

--- next.config.mjs
+++ next.config.mjs
@@ -24,7 +24,6 @@ const nextConfig = {
     unoptimized: true,
   },
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
 }
 
 export default withMDX(nextConfig)
