5afd6693d6919574111726fa97c3f300d1a3bdc4
Yorkie Makoto <yorkiefixer@gmail.com>
2023-09-27 20:46:38 +0800

    fix escaping issue

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,12 +7,14 @@ function SiteDocument() {
     externalScript = (
       <>
         <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env['GTAG']}`}></script>
-        <script>{`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){ dataLayer.push(arguments); }
-          gtag('js', new Date());
-          gtag('config', '${process.env['GTAG']}');
-        `}</script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){ dataLayer.push(arguments); }
+            gtag('js', new Date());
+            gtag('config', '${process.env['GTAG']}');
+          `
+        }}></script>
       </>
     )
   }
