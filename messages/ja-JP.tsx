f151a10a4711faa0c85747928d749961fdb7ef27
Yorkie Makoto <yorkiefixer@gmail.com>
2023-10-09 17:41:16 +0800

    update translations

--- messages/ja-JP.tsx
+++ messages/ja-JP.tsx
@@ -2,7 +2,7 @@ export default {
   "PageLayout": {
     "menuNames": {
       "download": "ダウンロード",
-      "document": "ドキュメント",
+      "document": "ドキュメンテーション",
       "community": "コミュニティ"
     },
     "menuSubItems": {
@@ -11,50 +11,55 @@ export default {
         "JSARCanary": "JSAR キャナリー"
       },
       "document": {
-        "manual": "開発者ガイド",
-        "apiReference": "API ドキュメント",
+        "manual": "マニュアル",
+        "apiReference": "API リファレンス",
         "publish": "公開",
-        "playground": "オンライン体験"
+        "playground": "プレイグラウンド"
       },
       "community": {
         "forum": "開発者フォーラム",
         "githubDiscussion": "GitHub ディスカッション"
       }
+    },
+    "footer": {
+      "languages": "言語",
+      "community": "コミュニティ"
     }
   },
   "LandingPage": {
-    "title": "スペースミニアプリ：スペースに埋め込めるWebランタイム",
+    "title": "JSAR: 埋め込み可能な空間のためのWebランタイム",
     "features": [
-      "Web標準API、TypeScript、Babylon.jsをサポート",
-      "スペースランタイムで実行され、相互に干渉しないスペースミニアプリ",
-      "アプリケーション層とレンダリング層のコードロジックを分離し、ネイティブに近いレンダリング性能を実現"
+      "Web 標準 API、TypeScript、Babylon.js をサポート",
+      "空間ベースのセキュリティ、アプリケーションは空間分離フレームワークで実行され、お互いに干渉しない",
+      "アプリケーションレイヤーを分離することで、ネイティブに近いレンダリング性能を実現"
     ],
     "tooltip": {
-      "XSML": "拡張可能な空間マークアップ言語（Extensible Spatial Marked Language）",
-      "SCSS": "スペースカスケーディングスタイルシート（Spatial Cascading Style Sheets）"
+      "XSML": "拡張可能な空間マークアップ言語",
+      "SCSS": "空間カスケーディングスタイルシート",
+      "comingSoon": "近日公開"
     },
     "buttons": {
-      "downloadVscodeExtension": "VSCode 拡張機能をダウンロード",
-      "developerDocumentation": "開発者ドキュメント",
-      "learnMore": "詳細を見る",
+      "downloadVscodeExtension": "Visual Studio Code 拡張機能",
+      "developerDocumentation": "開発者ドキュメンテーション",
+      "learnMore": "詳細情報",
       "usingTypeScriptInJSAR": "JSARでTypeScriptを使用する",
       "usingBabylonInJSAR": "JSARでBabylon.jsを使用する",
-      "applyForEarlyAccess": "JSAR UPMの早期アクセスを申請する"
+      "applyForEarlyAccess": "早期アクセスの申し込み"
     },
     "intro": {
       "title": [
-        "Webと同様",
-        "ただし、スペースミニアプリの開発用"
+        "Webのように",
+        "しかし、空間ミニアプリ用"
       ],
       "description": [
-        "スペースミニアプリは、最初のWeb開発体験を再現し、見たままになります。",
-        "<tooltip>XSML</tooltip>というHTMLに似た言語を導入し、コンポーネントのスペース構造を記述します。",
-        "<tooltip>SCSS</tooltip>と呼ばれるCSSに似た言語も導入され、コンポーネントのスペーススタイルを記述します。",
-        "同様に、JSARではスペースミニアプリのスクリプト言語としてTypeScript/JavaScriptが使用されます。"
+        "それは初期のWeb開発体験を復元し、見たままになります",
+        "それは <tooltip>XSML</tooltip> を導入しています、これはコンポーネントの3D構造を記述するためのHTMLのようなマークアップ言語です。",
+        "それは <tooltip >SCSS</tooltip> を導入しています、これはコンポーネントの3Dスタイルを記述するためのCSSのような言語です。",
+        "同様に、JSARはスクリプト言語としてTypeScript / JavaScriptを使用します。"
       ]
     },
     "typescript": {
-      "title": "組み込みのTypeScript",
+      "title": "TypeScript組み込み",
       "description": "JSARランタイムは、ミニアプリを実行する際にTypeScriptコードを直接解析できるため、開発者はTypeScriptコードをコンパイルせずに実行できます。",
       "features": [
         "簡単な設定でTypeScriptを使用できます",
@@ -62,5 +67,22 @@ export default {
         "TypeScriptソースコードを使用してアプリケーションを公開し、追加のビルドは不要"
       ]
     },
+    "babylonjs": {
+      "title": "<link>Babylon.js</link>を使用する",
+      "description": [
+        "Babylon.jsは、複数のバックエンド（WebGL、WebGPU、サーバーサイド、ネイティブ）をサポートするオープンソースの3Dレンダリングエンジンです。3Dゲームとアプリケーションを開発するためのゲームシーンAPIを提供しています。",
+        "JSARはBabylon.jsの一部のインターフェースを実装しており、開発者はBabylon.jsのAPIを直接使用できます（一部のサポートがあるかもしれません）。"
+      ]
+    },
+    "unity": {
+      "title": "<link>Unity</link>を使用して構築",
+      "description": [
+        "JSARを使用して開発された空間ミニアプリは、任意のUnityシーンで実行できます。JSAR UPM（Unity Package Manager）プラグインを提供しており、必要に応じて基本的なデータインターフェースを実装できます。これにより、UnityアプリケーションでJSAR空間ミニアプリを使用できます。"
+      ]
+    }
+  },
+  "ManualPages": {
+    "version": "バージョン",
+    "latest": "最新版",
   }
 }
--- messages/zh-TW.tsx
+++ messages/zh-TW.tsx
@@ -8,59 +8,81 @@ export default {
     "menuSubItems": {
       "download": {
         "vscodeExtension": "VSCode 擴充套件",
-        "JSARCanary": "JSAR Canary"
+        "JSARCanary": "JSAR 測試版"
       },
       "document": {
-        "manual": "開發者指南",
-        "apiReference": "API 文件",
+        "manual": "手冊",
+        "apiReference": "API 參考",
         "publish": "發佈",
         "playground": "線上體驗"
       },
       "community": {
         "forum": "開發者論壇",
-        "githubDiscussion": "Github 討論區"
+        "githubDiscussion": "GitHub 討論區"
       }
+    },
+    "footer": {
+      "languages": "語言",
+      "community": "社群"
     }
   },
   "LandingPage": {
-    "title": "空間小程式：可嵌入空間的 Web 運行時",
+    "title": "JSAR：可嵌入空間的 Web 執行時",
     "features": [
-      "支援 Web 標準 API、TypeScript 及 Babylon.js",
-      "空間運行安全，空間小程式運行在空間隔離框架中，互不干擾",
-      "透過應用層與渲染層代碼邏輯分離，實現接近原生的渲染性能"
+      "支援 Web 標準 API、TypeScript 和 Babylon.js",
+      "空間安全性，應用程式在空間隔離框架中運行，互不干擾",
+      "透過分離應用程式層實現接近原生的渲染效能"
     ],
     "tooltip": {
-      "XSML": "可擴展空間標記語言（Extensible Spatial Marked Language）",
-      "SCSS": "空間層級樣式表（Spatial Cascading Style Sheets）"
+      "XSML": "可擴展空間標記語言",
+      "SCSS": "空間層疊樣式表",
+      "comingSoon": "即將推出"
     },
     "buttons": {
-      "downloadVscodeExtension": "下載 VSCode 擴充套件",
+      "downloadVscodeExtension": "Visual Studio Code 擴充套件",
       "developerDocumentation": "開發者文件",
-      "learnMore": "瞭解更多",
+      "learnMore": "了解更多",
       "usingTypeScriptInJSAR": "在 JSAR 中使用 TypeScript",
       "usingBabylonInJSAR": "在 JSAR 中使用 Babylon.js",
-      "applyForEarlyAccess": "申請 JSAR UPM 試用"
+      "applyForEarlyAccess": "申請早期存取"
     },
     "intro": {
       "title": [
-        "如 Web 一般",
-        "但用於空間小程式開發"
+        "類似 Web",
+        "但針對空間小應用"
       ],
       "description": [
-        "空間小程式還原最初的 Web 開發體驗，所見即所得。",
-        "我們引入了 <tooltip>XSML</tooltip>，它是一種類似於 HTML 的語言，用於描述組件的空間結構。",
-        "我們引入了 <tooltip >SCSS</tooltip>，它是一種類似於 CSS 的語言，用於描述組件的空間樣式。",
-        "同樣地，JSAR 使用 TypeScript / JavaScript 作為空間小程式的腳本語言。"
+        "它恢復了最初的 Web 開發體驗，以所見即所得的方式呈現",
+        "引入了 <tooltip>XSML</tooltip>，這是一種類似 HTML 的標記語言，用於描述 3D 空間結構。",
+        "引入了 <tooltip >SCSS</tooltip>，這是一種類似 CSS 的語言，用於描述 3D 空間的風格。",
+        "同樣地，JSAR 使用 TypeScript / JavaScript 作為腳本語言。"
       ]
     },
     "typescript": {
       "title": "內建 TypeScript",
-      "description": "JSAR 運行時在執行小程式時，可直接解析 TypeScript 代碼，因此開發者無需任何編譯和配置即可運行 TypeScript 代碼。",
+      "description": "JSAR 執行時在執行小應用程式時，可直接解析 TypeScript 代碼，因此開發者無需任何編譯或配置。",
       "features": [
-        "簡單一步配置即可使用 TypeScript",
-        "在 Visual Studio Code 中享受 TypeScript 的智慧提示",
-        "透過 TypeScript 原始碼進行應用程式發佈，無需額外構建"
+        "使用簡單的一步配置使用 TypeScript",
+        "在 Visual Studio Code 中享受 TypeScript 的智慧建議",
+        "使用 TypeScript 原始碼發佈應用程式，無需額外建置"
       ]
     },
+    "babylonjs": {
+      "title": "使用 <link>Babylon.js</link>",
+      "description": [
+        "Babylon.js 是一個開源的 3D 渲染引擎，支援多個後端（WebGL、WebGPU、伺服器端和本地端）。它提供了一系列的遊戲場景 API，可用於開發 3D 遊戲和應用程式。",
+        "JSAR 實現了 Babylon.js <code>Engine</code> 的一些介面，允許開發者直接使用 Babylon.js 的 API 進行開發（儘管可能存在部分支援）。"
+      ]
+    },
+    "unity": {
+      "title": "使用 <link>Unity</link> 構建",
+      "description": [
+        "使用 JSAR 開發的空間小應用程式可以在任何 Unity 場景中運行。我們提供了 JSAR UPM（Unity 套件管理器）插件，允許您根據需要實現底層數據介面，以在您的 Unity 應用程式中使用 JSAR 空間小應用程式。"
+      ]
+    }
+  },
+  "ManualPages": {
+    "version": "版本",
+    "latest": "最新版"
   }
 }
