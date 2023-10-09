a6508e1437b248071d53d511f11fb6ceb01ed403
Yorkie Makoto <yorkiefixer@gmail.com>
2023-10-09 17:30:03 +0800

    fix build error for playground

--- pages/playground.tsx
+++ pages/playground.tsx
@@ -8,10 +8,7 @@ export default dynamic(() => import('../components/playground'), {
 export async function getStaticProps(context: NextPageContext) {
   return {
     props: {
-      // You can get the messages from anywhere you like. The recommended
-      // pattern is to put them in JSON files separated by locale and read
-      // the desired one based on the `locale` received from Next.js.
-      messages: (await import(`../messages/${context.locale}.json`)).default
+      messages: (await import(`../messages/${context.locale}`)).default
     }
   }
 }
